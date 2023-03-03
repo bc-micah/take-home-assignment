ed804f3bc02461348af9593d4ae794b358f7baae
Marco Loyo <marco.loyo@bigcommerce.com>
2023-03-03 15:44:07 -0600

    feat: simulate errors in api

--- mock-api/index.js
+++ mock-api/index.js
@@ -2,6 +2,11 @@ import express from "express";
 import fs from "fs";
 import path from "path";
 
+const ERROR_RATE = 0.1;
+const RESPONSE_DELAY_START = 10;
+const RESPONSE_DELAY_END = 1500;
+const random = (min = RESPONSE_DELAY_START, max = RESPONSE_DELAY_END) => Math.floor(Math.random() * (max - min)) + min;
+
 const countries = JSON.parse(fs.readFileSync(
   path.join('./mock-api/', "countries.json")
 ));
@@ -22,6 +27,20 @@ function getCountries(query, page, page_size) {
 }
 
 app.use("/flags", express.static("mock-api/flags"));
+
+app.use("/countries", [
+  (req, res, next) => {
+    if (Math.random() <= ERROR_RATE) {
+      throw new Error("Something went wrong");
+    }
+
+    next();
+  },
+  (req, res, next) => {
+    setTimeout(next, random());
+  }
+]);
+
 app.get("/countries", (req, res) => {
   // get the query parameters and set defaults if not defined
   const { query = "", page = 1, page_size = 10 } = req.query;
